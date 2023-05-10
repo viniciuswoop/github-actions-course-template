962b4a84306a2a048873b3e0100727f620515212
Vinicius Nascimento Santos <viniciuswoop@swoopfunding.com>
2023-05-10 16:46:30 +0100

    comit.

--- src/game.js
+++ src/game.js
@@ -1,7 +1,7 @@
 export default class Game {
   constructor (p1, p2) {
     this.p1 = p1
-    this.p2 = 'Bananas'
+    this.p2 = p2
     this.board = [[null, null, null], [null, null, null], [null, null, null]]
     this.player = Math.random() < 0.5 ? this.p1 : this.p2
     this.sym = 'X'
@@ -15,6 +15,7 @@ export default class Game {
   nextPlayer () {
     this.player = this.player === this.p1 ? this.p2 : this.p1
     this.sym = this.sym === 'X' ? 'O' : 'X'
+    
   }
 
   hasWinner () {
